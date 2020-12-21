b0aa97526aea8d62067c289c05832dd987918284
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2020-12-21 17:17:12 +0530

    Change image on click"

--- logic.py
+++ logic.py
@@ -1,2 +1,23 @@
-def row_col_gen(row_col):
-    pass
+import os
+from typing import List
+from random import shuffle
+
+
+def get_file_names_of_pics(folder: str,
+                           avoid: List[str] = [],
+                           extn: str = "") -> List[str]:
+    """
+    Get all the PNG file from a directory
+    :param folder: the path to the folder to look for files
+    :param avoid: the names of the files to be avoided
+    :param extn: The extension of the image file
+    """
+    file_names = [name for name in os.listdir(folder) if name not in avoid]
+    if extn:
+        file_names = [name for name in file_names if name.endswith(extn)]
+    return file_names
+
+
+def get_my_name(names: List[str]) -> str:
+    shuffle(names)
+    return names.pop() if names else ""
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from tkinter import *
 import pyglet
 from PIL import Image, ImageTk
 
+from logic import get_file_names_of_pics
+from logic import get_my_name
+
 pyglet.font.add_file("font/langar.ttf")
 
 
@@ -13,9 +16,13 @@ class Memorize(Tk):
         self.row_col = (5, 4)
 
         self.title("Memorize")
-        self.geometry("468x645+500+0")
+        self.geometry("446x610+500+0")
         self.buttons: Button = []
-        # self.resizable(False, False)
+        self.resizable(False, False)
+
+        # same as the file names for the images
+        self.button_titles = get_file_names_of_pics("./assets",
+                                                    avoid=["crown.png"]) * 2
 
         self.first_button_pressed: bool = False
         self.setup_ui()
@@ -43,7 +50,7 @@ class Memorize(Tk):
 
         # generate all the 20 buttons
         for _ in range(20):
-            btn = Button(self.board_frame, text="test")
+            btn = Button(self.board_frame, text="title")
             self.buttons.append(btn)
 
         # arrange all the buttons
@@ -51,25 +58,36 @@ class Memorize(Tk):
         col_count = 0
         for btn in self.buttons:
             btn.grid(row=row_count, column=col_count, padx=3, pady=3)
+            text = get_my_name(self.button_titles)
+            btn.config(command=lambda text=text, row_count=row_count, col_count
+                       =col_count, obj=btn, : self.button_pressed(
+                           obj, text, row_count, col_count),
+                       text=text)
 
             col_count += 1
             if col_count == self.row_col[1]:
                 col_count = 0
                 row_count += 1
 
-        # add the image
+        # add the default image image
         img = Image.open("assets/crown.png")
         img = img.resize((100, 100), Image.ANTIALIAS)
-        # image = PhotoImage(file="assets/crown.png")
         image = ImageTk.PhotoImage(img)
         for btn in self.buttons:
             btn.config(image=image)
             btn.image = image
 
         # verify the selection
-        def button_pressed(self, obj: Button, row: int, col: int) -> None:
-            if not self.first_button_pressed:
-                self.first_button_pressed = False
+    def button_pressed(self, obj: Button, title: str, row: int,
+                       col: int) -> None:
+        print(obj, title, row, col)
+        image = Image.open(f"./assets/{title}")
+        image = image.resize((100, 100), Image.ANTIALIAS)
+        image = ImageTk.PhotoImage(image)
+        obj.config(image=image)
+        obj.image = image
+        if not self.first_button_pressed:
+            self.first_button_pressed = False
 
 
 if __name__ == "__main__":
